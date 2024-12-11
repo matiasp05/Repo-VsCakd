eae5b46bbd9a37adf8995167cd455db9faa75d66
Matias Portela <matiasportela02@gmail.com>
2024-12-11 15:01:59 -0300

    Actualizar Burbujeo.c

--- Fundamentos de Programcion/Algoritmos/Ordenamiento/Burbujeo.c
+++ Fundamentos de Programcion/Algoritmos/Ordenamiento/Burbujeo.c
@@ -2,7 +2,7 @@
 
 void ordenar(Tvector vector, int n){
     int i, j, aux;
-    for(i=1;i<n;i++){
+    for(i=0;i<n;i++){
         for(j=0;j<n-i;j++){
             if(vector[j]>vector[j+1]){
                 aux = vector[j];
